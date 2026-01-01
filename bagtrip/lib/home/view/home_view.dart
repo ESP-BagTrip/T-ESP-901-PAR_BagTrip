d9046d289c01b27dada82468d96a89c2a26a5194
Mickael Payan <micka.payan@outlook.com>
2026-01-01 13:39:44 +0100

    Fix: transition and animation between page when carousel is using

--- bagtrip/lib/home/view/home_view.dart
+++ bagtrip/lib/home/view/home_view.dart
@@ -4,6 +4,7 @@ import 'package:bagtrip/home/view/home_hotel_form.dart';
 import 'package:bagtrip/home/view/home_other_form.dart';
 import 'package:bagtrip/home/widgets/home_top_cards.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -15,7 +16,8 @@ class HomeView extends StatefulWidget {
 class _HomeViewState extends State<HomeView> {
   late PageController _headerController;
   late PageController _bodyController;
-  int _currentIndex = 0;
+
+  bool _isHeaderActive = true;
 
   @override
   void initState() {
@@ -27,35 +29,49 @@ class _HomeViewState extends State<HomeView> {
       initialPage: initialPage,
     );
     _bodyController = PageController(initialPage: initialPage);
+
+    _headerController.addListener(_syncBodyToHeader);
+    _bodyController.addListener(_syncHeaderToBody);
   }
 
   @override
   void dispose() {
+    _headerController.removeListener(_syncBodyToHeader);
+    _bodyController.removeListener(_syncHeaderToBody);
     _headerController.dispose();
     _bodyController.dispose();
     super.dispose();
   }
 
+  void _syncBodyToHeader() {
+    if (_isHeaderActive &&
+        _headerController.hasClients &&
+        _bodyController.hasClients) {
+      _bodyController.jumpTo(_headerController.offset / 0.85);
+    }
+  }
+
+  void _syncHeaderToBody() {
+    if (!_isHeaderActive &&
+        _headerController.hasClients &&
+        _bodyController.hasClients) {
+      _headerController.jumpTo(_bodyController.offset * 0.85);
+    }
+  }
+
   void _onPageChanged(int index) {
-    if (_currentIndex == index) return;
-    setState(() => _currentIndex = index);
+    // No need to rebuild the UI on page change as the index is not used in build
+  }
 
-    if (_headerController.hasClients &&
-        _headerController.page?.round() != index) {
-      _headerController.animateToPage(
-        index,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
-    }
+  bool _onNotification(ScrollNotification notification, bool isHeader) {
+    if (notification.metrics.axis != Axis.horizontal) return false;
 
-    if (_bodyController.hasClients && _bodyController.page?.round() != index) {
-      _bodyController.animateToPage(
-        index,
-        duration: const Duration(milliseconds: 300),
-        curve: Curves.easeInOut,
-      );
+    if (notification is UserScrollNotification) {
+      if (notification.direction != ScrollDirection.idle) {
+        _isHeaderActive = isHeader;
+      }
     }
+    return false;
   }
 
   @override
@@ -64,34 +80,41 @@ class _HomeViewState extends State<HomeView> {
       headerSliverBuilder: (context, innerBoxIsScrolled) {
         return [
           SliverToBoxAdapter(
-            child: Column(
-              children: [
-                HomeTopCards(
-                  controller: _headerController,
-                  onPageChanged: _onPageChanged,
-                ),
-                const SizedBox(height: AppSize.boxSize16),
-              ],
+            child: NotificationListener<ScrollNotification>(
+              onNotification: (n) => _onNotification(n, true),
+              child: Column(
+                children: [
+                  HomeTopCards(
+                    controller: _headerController,
+                    onPageChanged: _onPageChanged,
+                  ),
+                  const SizedBox(height: AppSize.boxSize16),
+                ],
+              ),
             ),
           ),
         ];
       },
-      body: PageView.builder(
-        controller: _bodyController,
-        onPageChanged: _onPageChanged,
-        itemBuilder: (context, index) {
-          final formIndex = index % 3;
-          switch (formIndex) {
-            case 0:
-              return const HomeFlightForm();
-            case 1:
-              return const HomeHotelForm();
-            case 2:
-              return const HomeOtherForm();
-            default:
-              return const SizedBox();
-          }
-        },
+      body: NotificationListener<ScrollNotification>(
+        onNotification: (n) => _onNotification(n, false),
+        child: PageView.builder(
+          allowImplicitScrolling: true,
+          controller: _bodyController,
+          onPageChanged: _onPageChanged,
+          itemBuilder: (context, index) {
+            final formIndex = index % 3;
+            switch (formIndex) {
+              case 0:
+                return const HomeFlightForm();
+              case 1:
+                return const HomeHotelForm();
+              case 2:
+                return const HomeOtherForm();
+              default:
+                return const SizedBox();
+            }
+          },
+        ),
       ),
     );
   }
