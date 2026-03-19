df0f31cd9705ab3f3613ab95ec66bd27a72577fc
Aurelien-poline <aurelien.poline@epitech.eu>
2026-03-19 11:08:38 +0100

    pre commit for fork

--- api/tests/agent/test_state.py
+++ api/tests/agent/test_state.py
@@ -5,9 +5,6 @@ from src.agent.state import AgentState
 
 def test_agent_state_structure():
     """Test that AgentState has the expected fields."""
-    # Since it's a TypedDict (via MessagesState which usually is), we check keys if possible,
-    # or just instantiation if it were a class. 
-    # MessagesState from langgraph is a TypedDict.
     
     state: AgentState = {
         "messages": [],
